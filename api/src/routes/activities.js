86243765cdea60d1824e9b460ec71337e3fe20db
AgustinMillan <victor.agustin.millan@gmail.com>
2022-10-09 12:44:45 -0300

    backv1

--- api/src/routes/activities.js
+++ api/src/routes/activities.js
@@ -6,18 +6,28 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
-router.post("/", async(req,res)=>{
-    const {nombre, dificultad, duracion, temporada, paises} = req.body;
-    Actividades.create({
-        nombre,
-        dificultad,
-        duracion,
-        temporada
-    })
-    for (let i = 0; i < paises.length; i++) {
-        let matchP = await Countries.findAll({where: {nombre:paises[i]}});
-        Actividades.addCountries(matchP)
+router.post("/", async (req, res) => {
+  try {
+    const { nombre, dificultad, duracion, temporada, paises } = req.body;
+    const act = await Actividades.create({
+      nombre,
+      dificultad,
+      duracion,
+      temporada,
+    });
+
+    const paisesDB = await Countries.findAll();
+
+    for (let e = 0; e < paisesDB.length; e++) {
+      for (let i = 0; i < paises.length; i++) {
+        if (paisesDB[e].nombre.toLowerCase().includes(paises[i].toLowerCase()))
+          await act.addCountries(paisesDB[e]);
+      }
     }
-})
+    res.status(201).json([act, paises]);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
 module.exports = router;
